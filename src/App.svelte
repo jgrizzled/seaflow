df358dc0a4f421f0551922c5955f0f4c74a3741b
Marissa McDonnell <marissa.mcdonnell@fansided.com>
2021-12-29 17:38:41 -0700

    Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,19 @@
 <style>
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    font-size: 1em;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background-color: pink;
   }
 </style>
 
-<script lang="ts">
-  export let name: string;
+<script>
+  import Home from './components/Home.svelte';
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build
-    Svelte apps.
-  </p>
+  <Home />
 </main>
---
+++ src/components/Home.svelte
@@ -0,0 +1,28 @@
+<style>
+  #home {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    background-color: lavender;
+    padding: 1em;
+  }
+</style>
+
+<script lang="ts">
+  import Nav from './Nav.svelte';
+  import Sea from './Sea.svelte';
+
+  let currency = 'USD';
+
+  // eslint-disable-next-line
+  function handleCurrency(e: CustomEvent<any>) {
+    // eslint-disable-next-line
+    currency = e.detail.currency;
+  }
+</script>
+
+<div id="home">
+  <Nav on:currency={handleCurrency} />∏
+  <h1>Home screen</h1>
+  <Sea {currency} />
+</div>
